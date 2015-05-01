94166fd72c62e9de3dcdb8e139489e7761b7f796
Sasha Kovar <sasha-git@arcocene.org>
2015-05-01 10:26:59 -0700

    Rename all identifiers internally for wavefront-obj-mode

--- wavefront-obj-mode.el
+++ wavefront-obj-mode.el
@@ -6,99 +6,101 @@
 ;;
 ;;; Commentary:
 ;;
-;; A hodepodge of quick random hacks for editing wavefront obj files.
+;; A hodepodge of quick random hacks for editing Wavefront obj files.
 ;; Use as you wish.
 ;;
 
 ;;; Code:
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.obj\\'" . obj-mode))
+(add-to-list 'auto-mode-alist '("\\.obj\\'" . wavefront-obj-mode))
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.mtl\\'" . obj-mode))
+(add-to-list 'auto-mode-alist '("\\.mtl\\'" . wavefront-obj-mode))
 
-(defvar obj-mode-builtins-re (concat "^" (regexp-opt '("d" "f" "s"
-                                                       "v" "vn" "vp" "vt"
-                                                       "Ka" "Ke" "Kd" "Ks"
-                                                       "Ni" "Ns"
-                                                       "Tf" "Tr"
-                                                       "illum" "bump" "disp" "decal" "refl"
-                                                       "map_d"
-                                                       "map_Ka" "map_Ke" "map_Kd" "map_Ks"
-                                                       "map_Ni" "map_Ns"
-                                                       "map_bump"))))
+(defvar wavefront-obj-mode-builtins-re (concat "^" (regexp-opt '("d" "f" "s"
+                                                                 "v" "vn" "vp" "vt"
+                                                                 "Ka" "Ke" "Kd" "Ks"
+                                                                 "Ni" "Ns"
+                                                                 "Tf" "Tr"
+                                                                 "illum" "bump" "disp" "decal" "refl"
+                                                                 "map_d"
+                                                                 "map_Ka" "map_Ke" "map_Kd" "map_Ks"
+                                                                 "map_Ni" "map_Ns"
+                                                                 "map_bump"))))
 
-(defvar obj-mode-keywords
-  `((,obj-mode-builtins-re . font-lock-builtin-face)
+(defvar wavefront-obj-mode-keywords
+  `((,wavefront-obj-mode-builtins-re . font-lock-builtin-face)
     ("^\\(usemtl\\|mtllib\\|newmtl\\).*" . font-lock-string-face)
     ("^\\(o\\|g\\) .*" . font-lock-function-name-face)
     ("^#.*" . font-lock-comment-face)))
 
-(defvar obj-mode-defun-regex
+(defvar wavefront-obj-mode-defun-regex
   "^\\(g\\|o\\)\\s-+\\(.+\\)")
 
 ;; if we want separate submenus for objects and groups
-;; (defvar obj-imenu-generic-expression
+;; (defvar wavefront-obj-imenu-generic-expression
 ;;   '(("Objects"  "^o\\s-+\\(.*\\)" 1)
 ;;     ("Groups"  "^g\\s-+\\(.*\\)" 1)))
 
-(defvar obj-imenu-generic-expression
-  `((nil ,obj-mode-defun-regex 2)))
+(defvar wavefront-obj-imenu-generic-expression
+  `((nil ,wavefront-obj-mode-defun-regex 2)))
 
 ;;;###autoload
-(define-derived-mode obj-mode fundamental-mode "Obj"
+(define-derived-mode wavefront-obj-mode fundamental-mode "Obj"
   "Major mode for editing Wavefront obj ascii files.
 
-\\{obj-mode-map}"
-  :group 'obj
-  (setq font-lock-defaults '(obj-mode-keywords))
+\\{wavefront-obj-mode-map}"
+  :group 'wavefront-obj
+  (setq font-lock-defaults '(wavefront-obj-mode-keywords))
   (set (make-local-variable 'comment-start) "# ")
-  (set (make-local-variable 'paragraph-start) obj-mode-defun-regex)
+  (set (make-local-variable 'paragraph-start) wavefront-obj-mode-defun-regex)
   (set (make-local-variable 'beginning-of-defun-function)
-       'obj-beginning-of-defun)
+       'wavefront-obj-beginning-of-defun)
   (set (make-local-variable 'end-of-defun-function)
-       'obj-end-of-defun)
+       'wavefront-obj-end-of-defun)
   (set (make-local-variable 'add-log-current-defun-function)
-       'obj-current-defun)
+       'wavefront-obj-current-defun)
   (set (make-local-variable 'imenu-generic-expression)
-       obj-imenu-generic-expression))
+       wavefront-obj-imenu-generic-expression))
 
-(defun obj-try-to-add-imenu ()
+(defun wavefront-obj-try-to-add-imenu ()
   (condition-case nil (imenu-add-to-menubar "Imenu") (error nil)))
-(add-hook 'obj-mode-hook 'obj-try-to-add-imenu)
+(add-hook 'wavefront-obj-mode-hook 'wavefront-obj-try-to-add-imenu)
 
 
-(defun obj-beginning-of-defun (&optional arg)
+(defun wavefront-obj-beginning-of-defun (&optional arg)
   "Move backward to the beginning of a defun.
-Every 'g' or 'o' block is considered to be a defun (see `obj-mode-defun-regex').
-Return t unless search stops due to beginning or end of buffer."
+Every 'g' or 'o' block is considered to be a defun
+ (see `wavefront-obj-mode-defun-regex').  Return t unless
+search stops due to beginning or end of buffer."
   (interactive "p")
   (or arg (setq arg 1))
-  (obj-end-of-defun (- arg)))
+  (wavefront-obj-end-of-defun (- arg)))
 
-(defun obj-end-of-defun (&optional arg)
+(defun wavefront-obj-end-of-defun (&optional arg)
   "Move forward to the end of the current defun.
-Every 'g' or 'o' block is considered to be a defun (see `obj-mode-defun-regex').
+Every 'g' or 'o' block is considered to be a defun
+ (see `wavefront-obj-mode-defun-regex').
 Return t unless search stops due to beginning or end of buffer."
   (interactive "p")
   (or arg (setq arg 1))
 
-  (or (not (eq this-command 'obj-beginning-of-defun))
-      (eq last-command 'obj-end-of-defun)
+  (or (not (eq this-command 'wavefront-obj-beginning-of-defun))
+      (eq last-command 'wavefront-obj-end-of-defun)
       (and transient-mark-mode mark-active)
       (push-mark))
 
   (if (< arg 0)
-      (re-search-backward obj-mode-defun-regex nil t)
-      (re-search-forward obj-mode-defun-regex nil t)))
+      (re-search-backward wavefront-obj-mode-defun-regex nil t)
+      (re-search-forward wavefront-obj-mode-defun-regex nil t)))
 
-(defun obj-current-defun ()
-  "`add-log-current-defun-function' for Obj mode."
+(defun wavefront-obj-current-defun ()
+  "`add-log-current-defun-function' for Wavefront Obj mode."
   (save-excursion
-    (when (re-search-backward obj-mode-defun-regex nil t)
+    (when (re-search-backward wavefront-obj-mode-defun-regex nil t)
       (match-string 2))))
 
 
-(provide 'obj-mode)
+(provide 'wavefront-obj-mode)
 
-;;; obj-mode.el ends here
+;;; wavefront-obj-mode.el ends here
